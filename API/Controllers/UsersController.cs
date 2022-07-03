9f71e3992508bc18777e0615a5e61339ced7709f
HoNhatHy <nhathyy.12834@gmail.com>
2022-07-03 22:04:25 +0700

    section 3

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using API.Entities;
 using API.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Cors;
 
 namespace API.Controllers
 {
@@ -22,12 +23,14 @@ namespace API.Controllers
         }
 
         // one end point to get all the users from our database
+        [EnableCors("AnotherPolicy")]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() {
             return await _context.Users.ToListAsync();
         }
 
         // one end point to get a specific user from the database
+        [EnableCors("Policy1")]
         [HttpGet("{id}")] // api/users/id
         public async Task<ActionResult<AppUser>> GetUser(int id) {
             return await _context.Users.FindAsync(id);
--- API/Startup.cs
+++ API/Startup.cs
@@ -36,11 +36,13 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
             });
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
+        {   
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -52,6 +54,8 @@ namespace API
 
             app.UseRouting();
 
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+            
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
