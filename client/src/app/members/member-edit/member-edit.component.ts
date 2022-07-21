ba11deae89cc665b3265fc321d3210750771dace
nhathyorient <hy.ho@orientsoftware.com>
2022-07-21 09:11:51 +0700

    middle of section 10

---
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Member } from 'src/app/_models/member';
+import { User } from 'src/app/_models/user';
+import { AccountService } from 'src/app/_services/account.service';
+import { MembersService } from 'src/app/_services/members.service';
+import { Observable, take } from 'rxjs';
+
+@Component({
+  selector: 'app-member-edit',
+  templateUrl: './member-edit.component.html',
+  styleUrls: ['./member-edit.component.css'],
+})
+export class MemberEditComponent implements OnInit {
+  member: Member;
+  user: User;
+
+  constructor(
+    private accountService: AccountService,
+    private memberService: MembersService
+  ) {
+    this.accountService.currentUser$
+      .pipe(take(1))
+      .subscribe((user) => (this.user = user));
+  }
+
+  ngOnInit(): void {
+    this.loadMember();
+  }
+
+  loadMember() {
+    this.memberService
+      .getMemberByUsername(this.user.username)
+      .subscribe((member) => (this.member = member));
+  }
+}
