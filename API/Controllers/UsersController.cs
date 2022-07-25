863734eb908290b0f0bad4e5af3f107a639f4743
Ho Nhat Hy <hy.ho@orientsoftware.com>
2022-07-25 18:02:51 +0700

    byH

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -30,5 +30,6 @@ namespace API.Controllers
         public async Task<ActionResult<MemberDto>> GetUser(string username) {
             return await _userRepository.GetMemberAsync(username);
         }
+        
     }
 }
--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
 import { Member } from 'src/app/_models/member';
 import { User } from 'src/app/_models/user';
 import { AccountService } from 'src/app/_services/account.service';
 import { MembersService } from 'src/app/_services/members.service';
 import { Observable, take } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-member-edit',
@@ -11,12 +13,21 @@ import { Observable, take } from 'rxjs';
   styleUrls: ['./member-edit.component.css'],
 })
 export class MemberEditComponent implements OnInit {
+  @ViewChild('editForm') editForm: NgForm;
   member: Member;
   user: User;
+  @HostListener('window:beforeunload', ['$event']) unloadNotification(
+    $event: any
+  ) {
+    if (this.editForm.dirty) {
+      $event.returnValue = true;
+    }
+  }
 
   constructor(
     private accountService: AccountService,
-    private memberService: MembersService
+    private memberService: MembersService,
+    private toastr: ToastrService
   ) {
     this.accountService.currentUser$
       .pipe(take(1))
@@ -31,5 +42,10 @@ export class MemberEditComponent implements OnInit {
     this.memberService
       .getMemberByUsername(this.user.username)
       .subscribe((member) => (this.member = member));
+  }
+
+  updateMember() {
+    this.toastr.success('Profile updated successfully');
+    this.editForm.reset(this.member);
   }
 }
